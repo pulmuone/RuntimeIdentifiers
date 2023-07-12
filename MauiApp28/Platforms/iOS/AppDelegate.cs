ba81eb84bb2331e6bcf2559716d303a0839c96d4
hmji <hmji@nansoft.co.kr>
2023-07-12 13:12:18 +0900

    Add project files.

---
+++ MauiApp28/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiApp28;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
